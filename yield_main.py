38259a22dc852c0ebfc864a150e081834c617720
MoarSpaceFi <33559225+jordan-m-lee@users.noreply.github.com>
2018-04-10 20:13:20 -0600

    Update yield_main.py

--- yield_main.py
+++ yield_main.py
@@ -26,17 +26,13 @@ for file in os.listdir(path):
     fullname = path + "/" + file 
     with open(fullname) as fp:
        headers = []
-       cnt = 0
-       tr = 0
-       while cnt < 4:
+       for num in range(4):
            line = fp.readline()
            line = line.rstrip('\t')
            headers.append(line.rstrip('\n'))
-           cnt += 1
-       while tr < 4:
+       for num in range(4):
            holder = headers[tr].split()
            headers[tr] = float(holder[1])
-           tr += 1
        raw_data = np.loadtxt(fullname, skiprows = 5);
        uniqueness = time.ctime(os.path.getmtime(fullname)) 
        BIG.append(wave(uniqueness, headers[0], headers[1], headers[2], headers[3], raw_data, yield_average(raw_data)))
